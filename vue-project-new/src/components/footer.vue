856c7590e3ca098b3a5c53113632d441540847a3
easycrew <ieasycrew@163.com>
2017-08-05 15:36:56 +0800

    vue环境搭建

---
+++ vue-project-new/src/components/footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer">
+    <div class="readType" v-if="readType.count">{{readType.count}}</div>
+        <ul class="main-nav">
+            <li>
+                <router-link to="/home">
+                    <i class="icon-nav icon-nav1"></i><span>首页</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/quan" v-bind:class="{rrouter:activ}">
+                    <i class="icon-nav icon-nav2"></i><span>学友圈</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/friend">
+                    <i class="icon-nav icon-nav3"></i><span>学友</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/find">  
+                    <i class="icon-nav icon-nav4"></i><span>发现</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/mine">
+                    <i class="icon-nav icon-nav5"></i><span>我的</span>
+                </router-link>
+            </li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
